42285310e55f5aeb080cbeea02e16e7ac2ea9a08
manirsg <manirsg16@gmail.com>
2021-04-02 18:27:09 +0530

    update: useEffect initiation

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogsList from './BlogList'; 
 //* imported as plural just to understand that the import name can be different from export name
 const Home = () => {
@@ -8,9 +8,19 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
     const handleDelete = (id)=>{
         setBlogs(blogs.filter(blog=>blog.id!==id));
     }
+
+    //? useEffect: It will invoke the function provided as parameter whenever the component is rendered, even on initial render.
+    useEffect(()=>{
+        console.log("useEffect triggered");
+        console.log(blogs);
+        //! To be carefull when changing the state in useEffect, as it may end up in continuous loop... 
+        //? Explanation: (change useState in useEffect => component re-render => useEffect Triggered => change useState in useEffect and so on....)
+    })
+
     return (
         <div className="home">
             <BlogsList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
