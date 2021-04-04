56f139f6652e62a27d4efbcec9b2977d6b7960db
manirsg <manirsg16@gmail.com>
2021-04-04 15:59:10 +0530

    update: create blog post to json server

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -4,31 +4,52 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Mani');
+    
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //* default behaviour on a form submit is to reload the page
+        const blog = { title, body, author };
+        console.log(blog);
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('Blog added');
+            setIsPending(false);
+        }).catch(error=>{
+            console.error(error, "unable to add blog");
+            setIsPending(false);
+        })
+    }
     return (
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
                     required
-                    onChange={(event)=>setTitle(event.target.value)}
+                    onChange={(event) => setTitle(event.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea
                     type="text"
                     required
-                    onChange={(event)=>setBody(event.target.value)}
+                    onChange={(event) => setBody(event.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
                 <select
                     value={author}
-                    onChange={(event)=>setAuthor(event.target.value)}
+                    onChange={(event) => setAuthor(event.target.value)}
                 >
                     <option value="Mani">Mani</option>
                     <option value="Bala">Bala</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Adding blog...</button>}
             </form>
             <p>{title}</p>
             <p>{body}</p>
