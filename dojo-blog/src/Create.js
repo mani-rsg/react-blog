6e41071824be64a3fcb4b09e879e07632e880e77
manirsg <manirsg16@gmail.com>
2021-04-04 15:14:11 +0530

    update: add blog form with useState

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mani');
+    return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    onChange={(event)=>setTitle(event.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    type="text"
+                    required
+                    onChange={(event)=>setBody(event.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(event)=>setAuthor(event.target.value)}
+                >
+                    <option value="Mani">Mani</option>
+                    <option value="Bala">Bala</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
